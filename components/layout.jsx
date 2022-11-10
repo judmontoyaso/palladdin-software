097be29f0ed3153816fc70e11204154c9445e9c0
Judmontoyaso <juandavidsolorzano73@gmail.com>
2022-11-10 09:09:10 -0500

    avanzar en menú de navegación responsive

--- components/layout.jsx
+++ components/layout.jsx
@@ -2,18 +2,18 @@ import LanguageModal from "./languageModal";
 import Navbar from "./navbar";
 import SideBar from "./sideBar";
 
-import { Poppins } from "@next/font/google"
+import { Poppins } from "@next/font/google";
 
 const poppins = Poppins({
-  weight: '200',
-  subsets: ['latin']
-})
+  weight: "200",
+  subsets: ["latin"],
+});
 
 export default function Layout({ children }) {
   return (
     <div className={poppins.className}>
-      <div className="flex h-screen bg-background bg-cover">
-        <SideBar/>
+      <div className="flex h-fit sm:h-screen bg-background bg-cover">
+        <SideBar />
         <div className="flex flex-col w-full py-10 justify-between text-white bg-black/50">
           <Navbar />
           <main>{children}</main>
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,5 +1,8 @@
+import { faBars, faClose, faL } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { Router, useRouter } from "next/router";
+import { useState } from "react";
 import ActiveRoute from "./ActiveRoute";
 import { useEnglish } from "./English";
 
@@ -8,19 +11,41 @@ export default function Navbar() {
 
   const router = useRouter();
   console.log(router.pathname);
+  const [menuButton, setMenuButton] = useState(false);
+  const handleClick = () => {
+    setMenuButton(!menuButton);
+  };
+
   return (
-    <div className="flex justify-around px-10">
-      <ActiveRoute href="/">{english ? "Acerca" : "About"}</ActiveRoute>
-      <ActiveRoute href="/web">
-        {english ? "Diseño web" : "Web design"}
-      </ActiveRoute>
-      <ActiveRoute href="/mobile">
-        {english ? "Aplicaciones móviles" : "Mobile apps"}
-      </ActiveRoute>
-      <ActiveRoute href="/seo">SEO</ActiveRoute>
-      <ActiveRoute href="/contact">
-        {english ? "Contacto" : "Contact"}
-      </ActiveRoute>
-    </div>
+    <>
+      <button
+        className="float-left pr-3 pl-3 pt-1  hover:text-gray-600 rounded-sm lg:hidden text-white ml-auto outline-none justify-end   hover:text-xl fixed"
+        onClick={handleClick}
+      >
+        {!menuButton ? (
+          <FontAwesomeIcon icon={faBars} size="lg" />
+        ) : (
+          <FontAwesomeIcon icon={faClose} size="lg" />
+        )}
+      </button>
+
+      <div
+        className={`${
+          menuButton ? "block ml-10 absolute" : "hidden"
+        } lg:visible flex flex-col lg:flex-row justify-around mr-10 lg:flex lg:border-none border  sm:p-7 md:p-7 bg-white/90 lg:bg-transparent lg:text-white text-black lg:relative`}
+      >
+        <ActiveRoute href="/">{english ? "Acerca" : "About"}</ActiveRoute>
+        <ActiveRoute href="/web">
+          {english ? "Diseño web" : "Web design"}
+        </ActiveRoute>
+        <ActiveRoute href="/mobile">
+          {english ? "Aplicaciones móviles" : "Mobile apps"}
+        </ActiveRoute>
+        <ActiveRoute href="/seo">SEO</ActiveRoute>
+        <ActiveRoute href="/contact">
+          {english ? "Contacto" : "Contact"}
+        </ActiveRoute>
+      </div>
+    </>
   );
 }
